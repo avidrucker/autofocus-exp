66eb14e8d0593b51f6791da2aa8042e4915d2d83
Avi <avidrucker@gmail.com>
2019-12-30 16:04:09 -0500

    reconfig tslint

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ const APP_NAME = 'AutoFocus';
 exports.greetUser = (word = APP_NAME) => {
     return `Welcome to ${APP_NAME}!`;
 };
+// tslint:disable-next-line:no-console
 console.log(exports.greetUser());
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export const greetUser = (word: string = APP_NAME): string => {
   return `Welcome to ${APP_NAME}!`;
 }
 
+// tslint:disable-next-line:no-console
 console.log(greetUser());
