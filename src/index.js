f96d1e791751c724f5c59f29b29aefb41ef9c4dc
Avi Drucker <avidrucker@gmail.com>
2019-12-31 17:38:10 -0500

    confirm readline-sync usage success

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const readline_sync_1 = __importDefault(require("readline-sync"));
 const APP_NAME = 'AutoFocus';
 exports.greetUser = (word = APP_NAME) => {
     return `Welcome to ${APP_NAME}!`;
 };
 // tslint:disable-next-line:no-console
 console.log(exports.greetUser());
+// todo: remove manual test after running
+// Wait for user's response.
+const userName = readline_sync_1.default.question('May I have your name? ');
+// tslint:disable-next-line:no-console
+console.log('Hi ' + userName + '!');
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import readlineSync from 'readline-sync';
+
 const APP_NAME = 'AutoFocus';
 
 export const greetUser = (word: string = APP_NAME): string => {
@@ -6,3 +8,9 @@ export const greetUser = (word: string = APP_NAME): string => {
 
 // tslint:disable-next-line:no-console
 console.log(greetUser());
+
+// todo: remove manual test after running
+// Wait for user's response.
+const userName = readlineSync.question('May I have your name? ');
+// tslint:disable-next-line:no-console
+console.log('Hi ' + userName + '!');
