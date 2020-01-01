cd6e15abf0e063421f6d30c1a53fde6815e179c2
Avi Drucker <avidrucker@gmail.com>
2020-01-01 11:43:36 -0500

    config cross-env

--- src/index.js
+++ src/index.js
@@ -8,10 +8,40 @@ const APP_NAME = 'AutoFocus';
 exports.greetUser = (word = APP_NAME) => {
     return `Welcome to ${APP_NAME}!`;
 };
-// tslint:disable-next-line:no-console
-console.log(exports.greetUser());
-// todo: remove manual test after running
-// Wait for user's response.
-const userName = readline_sync_1.default.question('May I have your name? ');
-// tslint:disable-next-line:no-console
-console.log('Hi ' + userName + '!');
+const print = (s) => {
+    // tslint:disable-next-line:no-console
+    console.log(s);
+};
+var MainMenuChoice;
+(function (MainMenuChoice) {
+    MainMenuChoice["AddNew"] = "Add a New Todo";
+    MainMenuChoice["ReviewTodos"] = "Review & Dot Todos";
+    MainMenuChoice["EnterFocus"] = "Enter Focus Mode";
+    MainMenuChoice["ReadAbout"] = "Read About AutoFocus";
+    MainMenuChoice["Quit"] = "Quit Program";
+})(MainMenuChoice || (MainMenuChoice = {}));
+const menuChoices = [
+    MainMenuChoice.AddNew,
+    MainMenuChoice.ReviewTodos,
+    MainMenuChoice.EnterFocus,
+    MainMenuChoice.ReadAbout,
+    MainMenuChoice.Quit
+];
+const menuPrompt = 'What would you like to do?';
+const promptUserWithMainMenu = () => {
+    const selection = menuChoices[readline_sync_1.default.keyInSelect(menuChoices, menuPrompt, { cancel: false })];
+    // print(`Your menu choice was: ${selection}`);
+    return selection;
+};
+const main = () => {
+    print(exports.greetUser());
+    let running = true;
+    while (running) {
+        const answer = promptUserWithMainMenu();
+        if (answer === MainMenuChoice.Quit) {
+            running = false;
+        }
+    }
+    print("Have a nice day!");
+};
+main();
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,46 @@ export const greetUser = (word: string = APP_NAME): string => {
   return `Welcome to ${APP_NAME}!`;
 }
 
-// tslint:disable-next-line:no-console
-console.log(greetUser());
+const print = (s: string):void => {
+	// tslint:disable-next-line:no-console
+	console.log(s);
+}
 
-// todo: remove manual test after running
-// Wait for user's response.
-const userName = readlineSync.question('May I have your name? ');
-// tslint:disable-next-line:no-console
-console.log('Hi ' + userName + '!');
+enum MainMenuChoice {
+	AddNew = 'Add a New Todo',
+	ReviewTodos = 'Review & Dot Todos',
+	EnterFocus = 'Enter Focus Mode',
+	ReadAbout = 'Read About AutoFocus',
+	Quit = 'Quit Program'
+}
+
+const menuChoices: MainMenuChoice[] = [
+	MainMenuChoice.AddNew,
+	MainMenuChoice.ReviewTodos,
+	MainMenuChoice.EnterFocus,
+	MainMenuChoice.ReadAbout,
+	MainMenuChoice.Quit];
+	
+const menuPrompt = 'What would you like to do?';
+
+const promptUserWithMainMenu = (): MainMenuChoice => {
+	const selection: MainMenuChoice = menuChoices[readlineSync.keyInSelect(menuChoices, menuPrompt, {cancel: false})];
+	// print(`Your menu choice was: ${selection}`);
+	return selection;
+}
+
+const main = () => {
+	print(greetUser());
+
+	let running = true;
+	while(running) {
+		const answer = promptUserWithMainMenu();
+		if(answer === MainMenuChoice.Quit) {
+			running = false;
+		}
+	}
+	
+	print("Have a nice day!");
+}
+
+main();
