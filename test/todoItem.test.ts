1aa8ab6e8affc6b61305a2039558c96830c13645
Avi Drucker <avidrucker@gmail.com>
2020-01-05 15:41:56 -0500

    add todo item list print test stub

--- test/todoItem.test.ts
+++ test/todoItem.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { constructNewTodoItem, ITodoItem, TodoState } from '../src/todoItem';
 
 describe('Creating, adding a new item', () => {
-  it('should increase the todo item count by 1', () => {
+  	it('should increase the todo item count by 1', () => {
 		const todoList: ITodoItem[] = [];
 		const before:number = todoList.length;
 		const newItem: ITodoItem = constructNewTodoItem(
@@ -10,13 +10,25 @@ describe('Creating, adding a new item', () => {
 		todoList.push(newItem);
 		const after:number = todoList.length;
 		
-    expect(after).equals(before + 1);
+    	expect(after).equals(before + 1);
   });
 	
 	it('should create an item of state Unmarked', () => {
 		const newItem: ITodoItem = constructNewTodoItem(
 			"eat some cheese");
 		
-    expect(newItem.state).equals(TodoState.Unmarked);
-  });
+		expect(newItem.state).equals(TodoState.Unmarked);
+	  });
 });
+
+// TODO: evaluate necessity of print unit test
+describe.skip('Printing a todo item list', () => {
+	it('does nothing when there are no list items', () => {
+		// printTodoItemList()
+	})
+
+	it('correctly prints 3 lines when there are 3 todo items', () => {
+		// printTodoItemList()
+	})
+
+})
