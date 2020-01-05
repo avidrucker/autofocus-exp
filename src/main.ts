7f330e3676937685902cf178b78b7a288f8ffc5f
Avi Drucker <avidrucker@gmail.com>
2020-01-05 17:37:02 -0500

    implement list iteration with user input

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,10 @@ ENTER to quit: ";
 const newItemBodyPrompt = "Give your todo item a comment (ie. use \
 dishwasher for non-glass items) or hit ENTER key to skip: ";
 
+const promptUserForYNQ = (questionString: string): string => {
+	return readlineSync.question(questionString, {limit: ['y','n','q','Y','N','Q']}).toLowerCase();
+}
+
 const promptUserForNewTodoItem = (): ITodoItem | null => {
 	const headerText = readlineSync.question(newItemTitlePrompt, {
 		limit: /\w+/i,
@@ -69,6 +73,7 @@ export const main = ():void => {
 	print(greetUser());
 
 	const todoList: ITodoItem[] = [];
+	let cmwtd: string = "";
 
 	// todo_AD4: in 071, put main program loop inside of main function
 	// Next, dev extracts out pieces of main program loop into
@@ -87,8 +92,56 @@ export const main = ():void => {
 		}
 
 		if(answer === MainMenuChoice.ReviewTodos) {
+			// todo_AD6: in 095, implement review of todo items where
+			// it is assumed that 2 or more items exist in todo list
+			// Next, dev implements safety guards which prevent review
+			// of todos from occurring where there are 0 or 1 items only,
+			// instead, printing a message "You have no todo items yet!"
+			// or "Marking the only todo item in your list 'todo item title'",
+			// respectively.
+		
 			print("Your Todo List:")
 			printTodoItemList(todoList);
+			
+			// following the AutoFocus algorithm
+			// step 1: dot the first item
+			todoList[0].state = TodoState.Marked;
+			cmwtd = todoList[0].header; // Q_AD_001 // CMWTD is initialized to first item
+			
+			// todo_AD7: in 095, in 095, implement review of todo items where
+			// it is assumed that 2 or more items exist in todo list
+			// Next, dev implements non-acceptable answer to be interpretted
+			// as a 'do you want to quit?' option to accomodate users who
+			// have either malfactioning keyboards or physical impairments.
+			
+			// step 2: for each item after the first, the user is asked,
+			// do you want to do list[current index + 1] more than 
+			// list[current_index]? to which they can answer yes, no, or
+			// quit ('Y','N','Q').
+			
+			for(let i = 0; i < todoList.length - 1; i++) {
+				const current = todoList[i].header;
+				const next = todoList[i+1].header;
+				const ans = promptUserForYNQ(`Do you want to '${next}' more than '${cmwtd}'? (Y/N/Q) `);
+				// 'y','n','q'
+				if(ans === 'y') {
+					todoList[i+1].state = TodoState.Marked;
+					print(`Marking '${todoList[i+1].header}'...`);
+					cmwtd = todoList[i+1].header; // Q_AD_001: question to architect: ought this be uuid? todo: post question as issue
+					print(`Setting current most want to do to '${todoList[i+1].header}'.`);
+				}
+				if(ans === 'n') {
+					print(`Understood.`)
+				}
+				if(ans === 'q') {
+					print('Discontinuing the review process early...')
+					break;
+				}
+			}
+			
+			print(`You have finished reviewing ${todoList.length} items!`)
+			print(`Your current most want to do is '${cmwtd}'.`);
+			
 		}
 
 		if(answer === MainMenuChoice.Quit) {
