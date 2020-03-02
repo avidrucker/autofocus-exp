01435e7d4937152471b0c8068d4a2833382277a0
Avi <avidrucker@gmail.com>
2020-03-01 20:37:38 -0500

    feat add focus mode unready test 2

--- test/main.test.ts
+++ test/main.test.ts
@@ -161,12 +161,22 @@ describe('Conducting list iteration', () => {
 	});
 });
 
-describe('Entering focus mode when there are no todo items', ()=> {
-	it('does not affect the todoList or cmwtd', () => {
+describe('Entering focus mode', ()=> {
+	it('when there are no todo items does not affect the todo list or cmwtd', () => {
 		let todoList: ITodoItem[] = [];
 		let cmwtd = "";
 		[todoList, cmwtd] = conductFocus(todoList, cmwtd, {workLeft: 'y'}); // "There are no todo items."
 		expect(todoList.length).equals(0);
+		expect(cmwtd).equals("");
+	});
+
+	it('when there are no marked items doesn\'t affect the todo list of cmwtd', () => {
+		let todoList: ITodoItem[] = [];
+		let cmwtd = "";
+		const item1: ITodoItem = constructNewTodoItem("apple");
+		todoList = addTodoToList(todoList,item1);
+		[todoList, cmwtd] = conductFocus(todoList, cmwtd, {workLeft: 'y'}); // "The CMWTD has not been set."
+		expect(todoList.length).equals(1);
 		expect(cmwtd).equals("");
 	})
 })
