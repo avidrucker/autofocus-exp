72d5172a39145c9ad7aaa0e02350dbe9b380868f
Avi <avidrucker@gmail.com>
2020-03-08 21:22:03 -0400

    fix broken review setup, export list funcs

--- src/review.ts
+++ src/review.ts
@@ -1,38 +1,18 @@
 import { isReady, ITodoItem, TodoState } from "./todoItem";
-import { indexOfItem, itemExists, lastIndexOfItem } from "./todoList";
+import { firstReady, itemExists } from "./todoList";
 
-// returns -1 if there are no unmarked items
-export const getFirstUnmarked = (todoList: ITodoItem[]): number => {
-	return indexOfItem(todoList, "state", TodoState.Unmarked);
-}
-
-// returns -1 if there are no marked items
-export const getFirstMarked = (todoList: ITodoItem[]): number => {
-	return indexOfItem(todoList, "state", TodoState.Marked);
-}
-
-// returns -1 if there are no marked items
-export const getLastMarked = (todoList: ITodoItem[]): number => {
-	return lastIndexOfItem(todoList, "state", TodoState.Marked);
-}
-
+// issue: Architect decides how to manage todo items in backend #108
 export const setupReview = (todoList: ITodoItem[], cmwtd: string): any => {
-	const firstUnmarked = getFirstUnmarked(todoList);
-	const firstMarked = getFirstMarked(todoList);
-	// short circuit func if there are no todos OR no ready to review todos
-	if(todoList.length === 0 || firstUnmarked === -1) {
+	const readyTodo = firstReady(todoList); // short circuit func if 0 todos OR not ready to review
+	if(todoList.length === 0 || readyTodo === -1) {
 		return [todoList, cmwtd];
 	}
-	// firstMarked is -1, firstUnmarked is 0 ==> mark Unmarked
-	// firstMarked is 0, firstUnmarked is 1 ==> do nothing
-	// firstMarked is 1, firstUnmarked is 0 ==> mark Unmarked
-	if(firstMarked === -1 || (firstUnmarked < firstMarked)) {
+	if(firstReady(todoList) !== -1) {
 		// FVP step 1: dot the first ready todo item (the first non-complete, non-archived item)
-		todoList[firstUnmarked].state = TodoState.Marked; // issue: Dev fixes issue where first item is perma-marked #116
+		todoList[readyTodo].state = TodoState.Marked;
 	}
-	// issue: Architect decides how to manage todo items in backend #108
 	if(cmwtd === "" || cmwtd === null) {
-		cmwtd = todoList[firstUnmarked].header; // CMWTD is initialized to first ready todo item if unset
+		cmwtd = todoList[readyTodo].header; // CMWTD is initialized to first ready todo item if unset
 	}
 	return [todoList, cmwtd];
 }
