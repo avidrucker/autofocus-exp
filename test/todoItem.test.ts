136d3e1d4b36e344c471e1fc69020a66f3a394c4
Avi <avidrucker@gmail.com>
2020-03-14 08:27:15 -0400

    feat make undot item tests

--- test/todoItem.test.ts
+++ test/todoItem.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { constructNewTodoItem, ITodoItem, TodoState } from '../src/todoItem';
+import { constructNewTodoItem, ITodoItem, TodoState, undot } from '../src/todoItem';
 
 describe('TODO ITEM TESTS', () => {
 	describe('Creating a new item', () => {
@@ -11,4 +11,29 @@ describe('TODO ITEM TESTS', () => {
 			expect(newItem.state).equals(TodoState.Unmarked);
 			});
 	});
+
+	describe('Undotting', () => {
+		it('should undot dotted items', () => {
+			// make one marked item
+			let aItem: ITodoItem = constructNewTodoItem("a");
+			aItem.state = TodoState.Marked;
+			aItem = undot(aItem); // run undot
+			expect(aItem.state).equals(TodoState.Unmarked);
+		})
+		it('should leave unmarked items as is', () => {
+			// make one unmarked item
+			let aItem: ITodoItem = constructNewTodoItem("a");
+			aItem = undot(aItem); // run undot
+			// confirm mark is as it was initially
+			expect(aItem.state).equals(TodoState.Unmarked);
+		})
+		it('should leave completed items as is', () => {
+			// make one completed item
+			let aItem: ITodoItem = constructNewTodoItem("a");
+			aItem.state = TodoState.Completed;
+			aItem = undot(aItem);// run undot()
+			// confirm mark is as it was initially
+			expect(aItem.state).equals(TodoState.Completed);
+		})
+	})
 })
