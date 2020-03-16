4340289c9ec51dcaf6fb00fcab7ab1e681edda0e
Avi <avidrucker@gmail.com>
2020-03-16 02:48:28 -0400

    fix rename, relocate tests, dirs

---
+++ unit/review.test.ts
@@ -0,0 +1,87 @@
+import { expect } from 'chai';
+
+import { readyToReview, setupReview } from '../src/review';
+import { ITodoItem, TodoState } from '../src/todoItem';
+import { getFirstUnmarked } from '../src/todoList';
+import { makeNItemArray, markAllAs } from './test-util';
+
+describe('REVIEW MODE UNIT TESTS', ()=> {
+	describe('Finding unmarked todos', () => {
+		it('when there is one item, returns the first unmarked item', () => {
+			const todoList: ITodoItem[] = makeNItemArray(1);
+			expect(todoList.length).equals(1);
+			expect(getFirstUnmarked(todoList)).equals(0);
+		})
+
+		it('when there are multiple items, returns the first unmarked item', () => {
+			const todoList: ITodoItem[] = makeNItemArray(2);
+			todoList[0].state = TodoState.Completed;
+			expect(getFirstUnmarked(todoList)).equals(1);
+		})
+	
+		it('returns -1 when there are no todos', () => {
+			const todoList: ITodoItem[] = makeNItemArray(0);
+			expect(getFirstUnmarked(todoList)).equals(-1);
+		});
+	
+		it('returns -1 when there are no unmarked todos', () => {
+			let todoList: ITodoItem[] = makeNItemArray(2);
+			todoList = markAllAs(todoList, TodoState.Completed);
+			expect(getFirstUnmarked(todoList)).equals(-1);
+		});
+	
+		it('when there are both marked and unmarked items, returns the unmarked item', () => {
+			const todoList: ITodoItem[] = makeNItemArray(2);
+			todoList[0].state = TodoState.Marked;
+			todoList[1].state = TodoState.Unmarked;
+			expect(getFirstUnmarked(todoList)).equals(1);
+		})
+	});
+	
+	// issue: Dev refactors WET code to be more DRY #248
+	describe('Ready to review check', () => {
+		it('determines list `[o][o][o]` NOT ready for review', () => {
+			let todoList: ITodoItem[] = makeNItemArray(3);
+			todoList = markAllAs(todoList, TodoState.Marked);
+			expect(readyToReview(todoList)).equals(false);
+		})
+
+		it('determines list `[x][x][x]` NOT ready for review', () => {
+			let todoList: ITodoItem[] = makeNItemArray(3);
+			todoList = markAllAs(todoList, TodoState.Completed);
+			expect(readyToReview(todoList)).equals(false);
+		})
+
+		it('determines list `[x][x][o]` NOT ready for review', () => {
+			let todoList: ITodoItem[] = makeNItemArray(3);
+			todoList = markAllAs(todoList, TodoState.Completed);
+			todoList[2].state = TodoState.Marked;
+			expect(readyToReview(todoList)).equals(false);
+		})
+
+		it('determines list `[x][o][ ]` ready for review', () => {
+			let todoList: ITodoItem[] = makeNItemArray(3);
+			let cmwtd: string = "";
+			let lastDone = "";
+			todoList[0].state = TodoState.Completed;
+			lastDone = todoList[0].header;
+			[todoList, cmwtd] = setupReview(todoList, cmwtd);
+			expect(cmwtd).equals("banana");
+			expect(readyToReview(todoList)).equals(true);
+			// todo: consider testing readyToReview w/ lastDone as param
+		})
+	
+		it('determines list `[x][ ][ ]` ready for review', () => {
+			const todoList: ITodoItem[] = makeNItemArray(3);
+			todoList[0].state = TodoState.Completed;
+			expect(readyToReview(todoList)).equals(true);
+		})
+	})
+});
+
+// issue: Dev removes commented out code snippets #249
+// describe('',()=> {
+// 	it('',()=>{
+// 		print();
+// 	})
+// })
--- unit/todoList.test.ts
+++ unit/todoList.test.ts
@@ -1,11 +1,10 @@
 import { expect } from 'chai';
 
-import { conductReviewsEpic, setupReview } from '../src/review';
 import { constructNewTodoItem, ITodoItem, TodoState } from '../src/todoItem';
-import { addTodoToList, indexOfItem, itemExists, listToMarks, makePrintableTodoItemList, undotAll } from "../src/todoList";
+import { addTodoToList, indexOfItem, itemExists, makePrintableTodoItemList } from "../src/todoList";
 import { makeNItemArray, markAllAs } from './test-util';
 
-describe('TODO LIST TESTS', () => {
+describe('TODO LIST UNIT TESTS', () => {
 	describe('Adding a new item to the list', () => {
 		it('should increase the todo item count by 1', () => {
 			let todoList: ITodoItem[] = [];
@@ -50,51 +49,9 @@ describe('TODO LIST TESTS', () => {
 			expect(makePrintableTodoItemList(todoList)).equals("[ ] make this app");
 		})
 	
-		// issue: Dev refactors WET code to be more DRY #248
 		it('correctly generates 3 lines when there are 3 todo items', () => {
-			let todoList: ITodoItem[] = [];
-			const item1: ITodoItem = constructNewTodoItem("apple");
-			const item2: ITodoItem = constructNewTodoItem("banana");
-			const item3: ITodoItem = constructNewTodoItem("cherry");
-			todoList = addTodoToList(todoList,item1);
-			todoList = addTodoToList(todoList,item2);
-			todoList = addTodoToList(todoList,item3);
-			
+			const todoList: ITodoItem[] = makeNItemArray(3);
 			expect(makePrintableTodoItemList(todoList).split("\n").length).equals(3);
-		})
-	})
-	
-	// issue: Dev renames, relocates as integration tests #247
-	describe('List to marks function', () => {
-		it('should return a list of items marked `[o] [ ]` for a given list', () => {
-			const todoList: ITodoItem[] = makeNItemArray(2);
-			todoList[0].state = TodoState.Marked;
-			expect(listToMarks(todoList)).equals("[o] [ ]");
-		})
-	})
-	
-	// issue: Dev renames, relocates as integration tests #247
-	describe('Conducting list iteration', () => {
-		it('should correctly update CMWTD for input `n, y` ', () => {
-			let todoList: ITodoItem[] = makeNItemArray(3);
-			let cmwtd = "";
-			const lastDone = "";
-			[todoList, cmwtd] = setupReview(todoList, cmwtd);
-			[todoList, cmwtd] = conductReviewsEpic(todoList, cmwtd, lastDone, ['n', 'y']);
-			expect(cmwtd).equals("cherry");
-		});
-	})
-
-	// issue: Dev renames, relocates as integration tests #247
-	describe('Undotting all items', () => {
-		it('undots list of dotted, undotted, & completed items', () => {
-			let todoList: ITodoItem[] = makeNItemArray(3);
-			todoList[0].state = TodoState.Completed;
-			todoList[1].state = TodoState.Marked;
-			todoList = undotAll(todoList);
-			expect(todoList[0].state).equals(TodoState.Completed);
-			expect(todoList[1].state).equals(TodoState.Unmarked);
-			expect(todoList[2].state).equals(TodoState.Unmarked);
 		})
 	})
 })
