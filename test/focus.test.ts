54a8e21f949a1f9dd0d0a0509750632a091e8956
Avi <avidrucker@gmail.com>
2020-03-16 01:12:20 -0400

    fix add todos

--- test/focus.test.ts
+++ test/focus.test.ts
@@ -8,32 +8,39 @@ import { addTodoToList, getLastMarked } from '../src/todoList';
 
 describe('FOCUS MODE TESTS', ()=> {
 	describe('Entering focus mode', ()=> {
-		it('when there are no todo items does not affect the todo list or cmwtd', () => {
+		it('when there 0 items does not affect the todo list, cmwtd, or lastDone', () => {
 			let todoList: ITodoItem[] = [];
 			let cmwtd = "";
-			[todoList, cmwtd] = conductFocus(todoList, cmwtd, {workLeft: 'y'}); // "There are no todo items."
+			let lastDone = "";
+			[todoList, cmwtd, lastDone] = conductFocus(todoList, cmwtd, lastDone, {workLeft: 'y'}); // "There are no todo items."
 			expect(todoList.length).equals(0);
 			expect(cmwtd).equals("");
+			expect(lastDone).equals("");
 		});
 
 		it('when there are no marked items doesn\'t affect the todo list of cmwtd', () => {
 			let todoList: ITodoItem[] = [];
 			let cmwtd = "";
+			let lastDone = "";
 			const item1: ITodoItem = constructNewTodoItem("apple");
 			todoList = addTodoToList(todoList,item1);
-			[todoList, cmwtd] = conductFocus(todoList, cmwtd, {workLeft: 'y'}); // "The CMWTD has not been set."
+			[todoList, cmwtd, lastDone] = conductFocus(todoList, cmwtd, lastDone, {workLeft: 'y'}); // "The CMWTD has not been set."
 			expect(todoList.length).equals(1);
 			expect(cmwtd).equals("");
 		})
 	});
+
+	// todo: test creation of duplicate todos from answering yes to workLeft
 	
 	describe('Finding marked todos', () => {
 		it('returns the last marked item', () => {
 			let todoList: ITodoItem[] = [];
+			// todo: DRY this code
 			const item1: ITodoItem = constructNewTodoItem("apple");
 			const item2: ITodoItem = constructNewTodoItem("banana");
 			todoList = addTodoToList(todoList,item1);
 			todoList = addTodoToList(todoList,item2);
+			// todo: DRY this code
 			todoList[0].state = TodoState.Marked;
 			todoList[1].state = TodoState.Marked;
 			expect(getLastMarked(todoList)).equals(1);
@@ -46,6 +53,7 @@ describe('FOCUS MODE TESTS', ()=> {
 
 		it('returns -1 index when there are no marked todos', () => {
 			let todoList: ITodoItem[] = [];
+			// todo: DRY this code
 			const item1: ITodoItem = constructNewTodoItem("apple");
 			const item2: ITodoItem = constructNewTodoItem("banana");
 			todoList = addTodoToList(todoList,item1);
@@ -55,6 +63,7 @@ describe('FOCUS MODE TESTS', ()=> {
 
 		it('when there are both marked and unmarked items, returns the marked item', () => {
 			let todoList: ITodoItem[] = [];
+			// todo: DRY this code
 			const item1: ITodoItem = constructNewTodoItem("apple");
 			const item2: ITodoItem = constructNewTodoItem("banana");
 			todoList = addTodoToList(todoList,item1);
@@ -70,16 +79,19 @@ describe('FOCUS MODE TESTS', ()=> {
 			let todoList: ITodoItem[] = [];
 			const item1: ITodoItem = constructNewTodoItem("apple");
 			let cmwtd = "apple";
+			let lastDone = "";
 			todoList = addTodoToList(todoList,item1);
 			todoList[0].state = TodoState.Marked;
-			[todoList, cmwtd] = conductFocus(todoList, cmwtd, {workLeft:'n'});
+			[todoList, cmwtd, lastDone] = conductFocus(todoList, cmwtd, lastDone, {workLeft:'n'});
 			expect(todoList[0].state).equals(TodoState.Completed);
 			expect(todoList.length).equals(1);
 			expect(cmwtd).equals("");
+			// todo: test lastDone
 		})
 
 		it('updates CMWTD from last marked item to the previous marked', () => {
 			let todoList: ITodoItem[] = [];
+			// todo: DRY this code
 			const item1: ITodoItem = constructNewTodoItem("apple");
 			const item2: ITodoItem = constructNewTodoItem("banana");
 			todoList = addTodoToList(todoList,item1);
@@ -87,10 +99,12 @@ describe('FOCUS MODE TESTS', ()=> {
 			todoList[0].state = TodoState.Marked;
 			todoList[1].state = TodoState.Marked;
 			let cmwtd = "banana";
-			[todoList, cmwtd] = conductFocus(todoList, cmwtd, {workLeft:'n'});
+			let lastDone = "";
+			[todoList, cmwtd, lastDone] = conductFocus(todoList, cmwtd, lastDone, {workLeft:'n'});
 			expect(todoList[1].state).equals(TodoState.Completed);
 			expect(todoList.length).equals(2);
 			expect(cmwtd).equals("apple");
+			// todo: test lastDone
 		})
 	})
 });
