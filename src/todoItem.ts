b2fcc4194d8c86229033f5b3ce7d14a6ae911371
Avi <avidrucker@gmail.com>
2020-03-18 16:13:12 -0400

    fix refactor getMark, comment out unused states

--- src/todoItem.ts
+++ src/todoItem.ts
@@ -1,27 +1,29 @@
 export enum TodoState {
 	Unmarked,
 	Marked,
-	Completed,
-	Archived
+	Completed
+	// Archived,
+	// Null,
+	// Error
 }
 
 export interface ITodoItem {
 	body?: string|null;
-	created: string;
+	// created: string;
 	header: string;
-	modified: string;
+	// modified: string;
 	state: TodoState;
-	uuid: string;
+	// uuid: string;
 }
 
 export const constructNewTodoItem = ( headerText:string, bodyText:string="", stateIn:TodoState=TodoState.Unmarked): ITodoItem => {
 	const newItem: ITodoItem = {
 			body: bodyText,
-			created:"temp_created_date",
+			// created:"temp_created_date",
 			header: headerText,
-			modified:"temp_created_date",
-			state: stateIn,
-			uuid:"temp_unique_universal_identifier"
+			// modified:"temp_created_date",
+			state: stateIn
+			// uuid:"temp_unique_universal_identifier"
 	}
 	return newItem;
 }
@@ -29,31 +31,24 @@ export const constructNewTodoItem = ( headerText:string, bodyText:string="", sta
 const MARKED = 'o';
 const NO_MARK = ' ';
 const DONE = 'x';
-const ARCHIVED = 'A';
-const ERROR = '!';
+// const ARCHIVED = 'A';
+// const ERROR = '!';
+// const NULL = '?';
 
-// issue: Dev refactors multi if blocks #125
-export const getMark = (i: ITodoItem): string => {
-	if(i.state === TodoState.Marked) {
-		return `[${MARKED}]`;
-	}
-	if(i.state === TodoState.Unmarked) {
-		return `[${NO_MARK}]`;
-	}
-	if(i.state === TodoState.Completed) {
-		return `[${DONE}]`;
-	}
-	if(i.state === TodoState.Archived) {
-		return `[${ARCHIVED}]`;
-	}
-	return `[${ERROR}]`; // error state indicator
+export const getMark: any = {
+	[TodoState.Marked] : () => `[${MARKED}]`,
+	[TodoState.Unmarked] : () => `[${NO_MARK}]`,
+	[TodoState.Completed] : () => `[${DONE}]`,
+	// [TodoState.Archived] : () => `[${ARCHIVED}]`,
+	// [TodoState.Null] : () => `[${NULL}]`,
+	// [TodoState.Error] : () => `[${ERROR}]`
 }
 
 export const stringifyTodoItem = (i : ITodoItem): string => {
-	return `${getMark(i)} ${i.header}`;
+	return `${getMark[i.state]()} ${i.header}`;
 }
 
-// ready for review
+// "todo item is ready for review"
 export const isReady = (i: ITodoItem): boolean => {
 	return i.state === TodoState.Unmarked || i.state === TodoState.Marked;
 }
@@ -63,10 +58,7 @@ export const setState = (i: ITodoItem, newState: TodoState ): ITodoItem => {
 	return constructNewTodoItem(i.header, "", newState);  // note: this creates a NEW todo item
 }
 
+// note: this creates a NEW todo item
 export const undot = (i: ITodoItem): ITodoItem => {
-	if(i.state === TodoState.Marked) {
-		return constructNewTodoItem(i.header); // note: this creates a NEW todo item
-	} else {
-		return i;
-	}
+	return i.state === TodoState.Marked ? constructNewTodoItem(i.header) : i;
 }
