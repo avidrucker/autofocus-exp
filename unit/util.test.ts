7faff712daf6a8c591b0750912bf43af79f413fd
Avi <avidrucker@gmail.com>
2020-03-18 15:19:07 -0400

    feat test new util funcs

--- unit/util.test.ts
+++ unit/util.test.ts
@@ -1,6 +1,6 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 
-import { getPluralS } from '../src/util';
+import { getMinFrom0Up, getPluralS, isDefinedString, isNeg1 } from '../src/util';
 
 describe('UTILITY TESTS', () => {
 	describe('Pluralization of words', ()=> {
@@ -24,5 +24,49 @@ describe('UTILITY TESTS', () => {
 			const suffix: string = getPluralS(count);
 			expect(`${count} ${word}${suffix}`).equals("5 cups");
 		})
-	})
+	});
+
+	describe('isDefinedString',()=>{
+		it('returns false when variable is an empty string',()=>{
+			const emptyString: string = "";
+			expect(isDefinedString(emptyString)).equals(false);
+		});
+
+		it('returns true when variable has a non-empty, non-null value',()=>{
+			const boy: string = "Harry";
+			expect(isDefinedString(boy)).equals(true);
+		})
+	});
+
+	describe('isNeg1',()=>{
+		it('returns true when -1 argument is passed in',()=>{
+			expect(isNeg1(-1)).equals(true);
+		})
+
+		it('returns false when 5 argument is passed in',()=>{
+			expect(isNeg1(5)).equals(false);
+		})
+	});
+
+	// getMinFrom0Up
+	describe('getMinFrom0Up',()=>{
+		describe('returns the lower of two numbers that are 0 or higher', () => {
+			it('giving -1 back when (-1,-1) is passed in',()=>{
+				expect(getMinFrom0Up(-1,-1)).equals(-1);
+			})
+			it('giving 7 back when (-1,7) is passed in',()=>{
+				expect(getMinFrom0Up(-1,7)).equals(7);
+			})
+			it('giving 2 back when (2,-1) is passed in',()=>{
+				expect(getMinFrom0Up(2,-1)).equals(2);
+			})
+			it('giving 3 back when (3,20) is passed in',()=>{
+				expect(getMinFrom0Up(3,20)).equals(3);
+			})
+			it('throws an error when below range number is passed in',()=>{
+				assert.throw(() => getMinFrom0Up(-5, 1), 
+					Error, "Bad input error thrown");
+			});
+		})
+	});
 })
