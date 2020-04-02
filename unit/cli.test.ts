b138871b3d592792b0df981fd4450f21275dbbbb
Avi <avidrucker@gmail.com>
2020-04-02 12:25:06 -0400

    update linting fixes

--- unit/cli.test.ts
+++ unit/cli.test.ts
@@ -1,8 +1,8 @@
 // issue: Dev determines way to test cli functions, then writes those tests #284
 import { expect } from "chai";
 
-describe('test suite',()=>{
-	it('test case',()=>{
-		expect(false).equals(true);
-	})
+describe("test suite", () => {
+  it("test case", () => {
+    expect(false).equals(true);
+  });
 });
